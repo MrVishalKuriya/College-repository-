52a8a7fc2b1f9c23135af147726b56dd1fc5e001
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-31 14:53:25 +0530

    ok

---
+++ WhatsApp_Cloan/whatsapp/whatsapp/src/app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { getRouteInfoFromState } from "expo-router/build/LocationProvider";
+
+const AUTHStack = () => {
+    return (
+        <view>
+            <Text>AUTHStack</Text>
+        </view>
+    );
+};
+
+expect default AUTHStack;
